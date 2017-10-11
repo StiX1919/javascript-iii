d20880d9549c673068d4b3958ff88c5115354e0b
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-10-11 15:56:10 -0500

    almost done

--- destructuring.js
+++ destructuring.js
@@ -12,7 +12,7 @@ var carDetails = {
 }
 
 // CODE HERE
-
+var {color,make,model,year}=carDetails
 
 // ========================
 
@@ -20,7 +20,7 @@ var carDetails = {
 // In the function below named greeting, it is receiving an object as a parameter. Use object destructuring to save the object properties to new variables. The property names are firstName, lastName, and title. Return the concatenated string.
 
 function greeting( obj ) {
-  // CODE HERE
+  var {firstName,lastName,title}=obj// CODE HERE
   
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
@@ -30,7 +30,10 @@ function greeting( obj ) {
 
 
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
-
+function totalPopulation(obj){
+  var{utah,california,texas,arizona}=obj
+  return utah+california+texas+arizona
+}
   // CODE HERE
 
 
@@ -38,7 +41,12 @@ function greeting( obj ) {
 
 
 // Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
-
+function ingredients(object){
+  newArr = []
+  var{carb,fat,protein}=object
+  newArr.push(carb,fat,protein)
+  return newArr
+}
   // CODE HERE
 
 
@@ -52,7 +60,9 @@ function greeting( obj ) {
 // }
 
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
-
+function largeNumbers({first,second,third}){
+  return Math.min(first,second,third)
+}
   // CODE HERE
 
 
