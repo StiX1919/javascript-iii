22dfbfc7bf8315ba7e322ed6daa6eb7c7b5b8368
Spencer Smith <stix1919@hotmail.com>
2018-08-15 16:44:28 -0500

    grading

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -1,172 +1,201 @@
-// ========================
-// FOR IN LOOPS & OBJECT DELETE
-// ========================
+/*
+  Once you complete a problem, refresh ./for-in-delete.html in your browser and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
+  You can refresh the page at any time to re-run all the tests.
+*/
 
+/*
+  First we'll look at the difference between accessing property values in
+  a for in loop and accessing the property name in a for in loop.
+  In the example below, we are accessing the property values. 
+  Uncomment the code below, run it and look at what prints in the console.
+*/
 
-// First we'll look at the difference between accessing property values in a for in loop and accessing the property name in a for in loop.
+////////// PROBLEM 1 //////////<<<<<COMPLETE
+/*
+  Inside the function showValues, write a for in loop that concatenates
+   each of the property values and returns the concatenated string.
+*/
+var objProblem1 = {
+  one: 'Duck',
+  two: ' goes',
+  three: ' quack',
+  four: ' quckity',
+  five: ' quack.'
+}
+var hold = "";
+function showValues(objProblem1) {
+for(var prop in objProblem1){
+hold += objProblem1[prop];
+}
+return hold;
+}
+console.log(showValues(objProblem1))
 
-// In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
-
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
-
-// for(var key in values) {
-//   console.log(values[key])
-// }
-
-// In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
-
-// for(var key in values) {
-//   console.log(key)
-// }
-
-
-// ========================
-
-
-// Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
-
-function showValues( obj ) {
-  var string = ""
-  for(let i in obj){
-    string += obj[i]
-    console.log(obj[i])
+////////// PROBLEM 2 //////////<<<<<COMPLETE
+/*
+  Write a function called greaterThan10 that takes in an object. 
+  Write a for in loop that loops over the object and changes any value
+   that is great than 10 to 0. 
+  Return the updated object.
+*/
+var obj = {
+  one:  1,
+  two:  25,
+  three:  3,
+  four:  10,
+  five:  15,
+  six:  55,
+}
+function greaterThan10(list)
+{
+  for (var prop in list){
+    if (list[prop] > 10){
+      list[prop] = 0;
+     }
+}
+return list;
+}
+greaterThan10(obj)
+////////// PROBLEM 3 //////////<<<<<COMPLETE
+/*
+  Write a function called double that takes in an object.
+  Write a for in loop that loops over the object and changes every
+  value to be itself multipled by 2.
+  Return the updated object.
+*/
+var toBeDoubled = {
+num1: 2,
+num2: 5,
+num3: 7
+}
+var a = 0;
+function double(toBeDoubled)
+{
+  for (prop in toBeDoubled){
+   toBeDoubled[prop] *= 2
   }
-  return string // CODE HERE
+  return toBeDoubled;
+}
+ console.log(double(toBeDoubled));
+
+ ////////// PROBLEM 4 //////////<<<<<COMPLETE
+/*
+  Write a function called secrets that will take in an object.
+  Create an empty string variable.
+  Write a for in loop that loops over the object.
+  If the property name starts with an 'sh', concatenate the value to the string variable.
+  By the end of the for in loop, you should have a sentence, return that sentence.
+*/
+var objectProblem4 = {
+str1: "",
+str2: "",
+str3: ""
+}
+var empty = "";
+function secrets(obj)
+{
+  for(var values in obj)
+  {
+    if (values)
+    sentence = values + empty;
+  }
+  return sentence;
+}
+secrets(objectProblem4);
+/* 
+  Sometimes it's needed to delete object properties. 
+  All you need is the word delete before a reference to the object property value. 
+  Uncomment the example below to see a for in loop deleting all the properties inside an object.
+*/
+
+ var deleteAllThethings = {
+   one: 1,
+   two: 2,
+   three: 3
+ }
+
+ for(var key in deleteAllThethings) {
+   delete deleteAllThethings[key]
+ }
+////////// PROBLEM 5 //////////<<<<<COMPLETE
+/*
+  Write a function called removePassword that takes in an object.
+  Delete the property password and return the object.
+*/
+ var objectProblem5={
+  password: "",
+  notpassword: ""
+}
+function removePassword(obj)
+{
+  for(var key in obj) {
+    delete obj.password;
+    return obj;
+  }
 }
 
-
-// ========================
-
-
-// Write a function called greaterThan10 that takes in an object. Write a for in loop that loops over the object and changes any value that is great than 10 to 0. Return the updated object.
-
-// CODE HERE
-function greaterThan10(obj){
-  for (i in obj){
-    if(obj[i]>10){
-      obj[i]=0
-    }
-  }
-  return obj
-}
-
-
-// ========================
-
-
-// Write a function called double that takes in an object. Write a for in loop that loops over the object and changes every value to be itself multipled by 2. Return the updated object.
-
-// CODE HERE
-function double(obj){
-  for(i in obj){
-    obj[i]*=2
-  }
-  return obj
-}
-
-
-// ========================
-
-
-// Write a function called secrets that will take in an object. Create an empty string variable. Write a for in loop that loops over the object. If the property name starts with an 'sh', concatenate the value to the string variable. By the end of the for in loop, you should have a sentence, return that sentence.
-
-// CODE HERE
-function secrets(obj){
-  emSt = ""
-  for(i in obj){
-    if (i.indexOf('sh')===0){
-      emSt+=obj[i]
-    }
-  }
-  return emSt
-}
-
-// ========================
-
-
-// Sometimes it's needed to delete object properties. All you need is the word delete before a reference to the object property value. Uncomment the example below to see a for in loop deleting all the properties inside an object.
-
-// var deleteAllThethings = {
-//   one: 1,
-//   two: 2,
-//   three: 3
-// }
-
-// for(var key in deleteAllThethings) {
-//   delete deleteAllThethings[key]
-// }
-
-// console.log(deleteAllThethings)
-
-// ========================
-
-
-// Write a function called removePassword that takes in an object. Delete the property password and return the object.
-
-// CODE HERE
-function removePassword(obj){
-  for(i in obj){
-    if(i==='password')
-    delete obj[i]
-  }
-  return obj
-}
-
-
-
-// ========================
-
-
-// Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
-
+////////// PROBLEM 6 //////////
 var deleteTheBigNumbers = {
   first: 10,
   second: 20,
   third: 110,
   fourth: 200
 }
-for(i in deleteTheBigNumbers){
-  if(deleteTheBigNumbers[i]>100){
-    delete deleteTheBigNumbers[i]
+for (var value in deleteTheBigNumbers)
+{
+  if (deleteTheBigNumbers[value] > 100)
+  {
+    delete deleteTheBigNumbers[value];
   }
 }
-// CODE HERE
+////////// PROBLEM 7 //////////
 
-
-// ========================
-
-
-// Write a function called startsWithK that takes an object as a parameter. Write a for in loop to loop over the object. If any property name starts with k, delete that property. Return the updated object.
-
-// CODE HERE
-function startsWithK(obj){
-  for(i in obj){
-    if(i.indexOf("k")===0){
-      delete obj[i]
+/*
+  Write a function called startsWithK that takes an object as a parameter.
+  Write a for in loop to loop over the object.
+  If any property name starts with k, delete that property.
+  Return the updated object.
+*/
+var objectK = {
+  one: "kool",
+  two: "Cool",
+  three: "NotCool"
+}
+function startsWithK(objectK){
+  for (value in objectK){
+    if(objectK[value].charAt(0) === "k"){
+      delete objectK[value];
     }
+    return objectK;
   }
-  return obj
+}
+console.log(startsWithK(objectK))
+
+
+
+////////// PROBLEM 8 //////////
+
+/*
+  Write a function called hiddenTreasure that takes in an object.
+  Write a for in loop that loops over this object. Each property
+  will have a sentence as it's value.
+  If the property value does not contain the word 'treasure', delete the property.
+  Return the updated object.
+  (hint: the method includes() may be of use...)
+*/
+var objectProblem8 = {
+  daveyJonesLocker: "I contain fish",
+  mountOlympus: "I contain lightning",
+  fortKnocks: "I contain tanks",
+  theFederalReserve: "I contain treasure"
 }
 
-
-// ========================
-
-
-// Write a function called hiddenTreasure that takes in an object. Write a for in loop that loops over this object. Each property will have a sentence as it's value. If the property value does not contain the word 'treasure', delete the property. Return the updated object.
-// (hint: the method includes() may be of use...)
-
-// CODE HERE
-function hiddenTreasure(obj){
-  for(i in obj){
-    if(obj[i].includes("treasure")===false){
-      delete obj[i]
-    }
+function hiddenTreasure(objectProblem8){
+ for(var prop in objectProblem8){
+ if (objectProblem8[prop].search("treasure") == -1){delete objectProblem8[prop];}
+ else {return objectProblem8}
   }
-  return obj
 }
+  console.log(hiddenTreasure(objectProblem8));
--- higher-order.js
+++ higher-order.js
@@ -1,50 +1,77 @@
-// Filter, Map, Reduce
+/*
+  Once you complete a problem, refresh ./higher-order.html in your browser 
+  and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
+  You can refresh the page at any time to re-run all the tests.
+*/
 
-// Below we have an array of numbers
-// We want to filter down to just the even numbers
+////////// PROBLEM 1 //////////
 
-// The filter function takes a callback with the parameters
-// function(element, index, wholeArray){}  Function Form
-// (element, index, wholeArray)=>{}    Arrow Form
+// Do not edit the code below.
+// Do not edit the code above.
 
+/*
+  Use the filter method on mixedNumbers to make a new array of just the even numbers.
+  The filter function takes a callback with the parameters
+  function(element, index, wholeArray){}  Function Form
+  (element, index, wholeArray)=>{}    Arrow Form
+*/
 const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
+var evenArray = [];
+function isEven(num)
+{return (num % 2 == 0)}
+mixedNumbers.filter(isEven);
+console.log(mixedNumbers.filter(isEven));
+// <<< IMPLIMENT ME!
+//(element, index, wholeArray)=>{} 
+//let evenNumbers  = mixedNumbers.filter(cb)
 
-let evenNumbers = mixedNumbers.filter(even=>{return even%2===0}) // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
-// Below we have an array of prices.  We want to calculate their totals after
-// We want to use a map function on the prices to calculate a new array of
-// post tax prices.  We'll use a 7% tax rate.
+////////// PROBLEM 2 //////////
 
-// Math reminder times the price by 1 + taxRate as a decimal
-// ie (price * 1.07)
-
-// The map function also takes a callback with the parameters
-// function(element, index, wholeArray){}  Function Form
-// (element, index, wholeArray)=>{}    Arrow Form
-
+// Do not edit the code below.
 const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
+// Do not edit the code above.
 
-let postTaxPrices =prices.map(price=>{return price*1.07}) // = prices.map(/* Provide Your Callback Here );
+/*
+  Use the map method on the prices array to calculate a new array of post-tax prices.
+  Use a 7% tax rate.
+  Math reminder! To calculate the price after tax, multiply the price by 1
+   plus the taxRate as a decimal.
+  Example: if tax is 7%, the price afterTax could be calculated
+   like this: afterTax = price * 1.07) 
+  The map function also takes a callback with the parameters
+  function(element, index, wholeArray){}  Function Form
+  (element, index, wholeArray)=>{}    Arrow Form
+*/
+
+//Code Here
+let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+//>>>>>>>>WATCH ME ! https://www.youtube.com/watch?v=nicMAoW6u1g
+
+
+////////// PROBLEM 3 //////////
+
+// Do not edit the code below.
+const populations = [8175133, 3792621, 2695598, 2100263];
+// Do not edit the code above.
+
+/*
+  Use the reduce method to calculate the sum of all the populations in the array.
+  The reduce function has a slightly different setup for it's callback.
+  function(runningTotal, curElement, curIndex, wholeArray){} Function Form
+  (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
+*/
+
+//Code Here
+let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
-// Below we have an array of populations.
-// We want to use a reduce to calculate the sum of all the populations.
+////////// PROBLEM 4 //////////
 
-// The reduce function has a slightly different setup for it's callback
-// function(runningTotal, curElement, curIndex, wholeArray){} Function Form
-// (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
-
-const populations = [8175133, 3792621, 2695598, 2100263]
-
-let totalPopulation = populations.reduce((pop,lat)=> {return pop + lat})  //  = populations.reduce(/* Provide Your Callback Here */)
-
-// Now we're going to ramp up these a little bit.  Instead of just arrays of numbers
-// We are going to have array of objects that we want to use map filter and reduce with
-
-// Below we have an array of totally unique monsters we want to use a filter
-// function to return only the monsters that have a CP of over 200
-
+// Do not edit the code below.
 const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulbabunny","CP":135},
 {"monster":"Bulbabunny","CP":250},{"monster":"Ponylopse","CP":277},{"monster":"Ponylopse","CP":184},
 {"monster":"Pikadoughnet","CP":207},{"monster":"Bulbabunny","CP":139},{"monster":"Pikadoughnet","CP":47},
@@ -53,35 +80,53 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 {"monster":"Pikadoughnet","CP":253},{"monster":"Sandmush","CP":146},{"monster":"Bulbabunny","CP":247},
 {"monster":"Charaflier","CP":55},{"monster":"Bulbabunny","CP":72},{"monster":"Pikadoughnet","CP":300},
 {"monster":"Sandmush","CP":262},{"monster":"Sandmush","CP":25},{"monster":"Charaflier","CP":215},
-{"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}]
+{"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}];
+// Do not edit the code above.
 
-let myStrongest = monstersInYourPocket.filter(power=>power.CP>200) // Your code here
+/*
+  Now we're going to ramp these up a little bit.
+  Instead of just arrays of numbers, we are going to have array of objects that
+   we want to use map, filter, and reduce with.
+  Use the filter method to return only the monsters that have a CP of over 200.
+*/
 
-// Below we have an array of orders.  but they all have different tax rates.
-// We want to make a new array of total prices.
+//Code Here
+let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
+
+
+////////// PROBLEM 5 //////////
+
+// Do not edit code below.
 const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
 {"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
 {"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
-{"price":48,"tax":0.13}]
+{"price":48,"tax":0.13}];
+// Do not edit code above.
 
-let ordersTotal = orders.map( order => order.totalPrice = orders.price + (orders.price*orders.tax)) // Your code here
+/*
+  Use a higher order method to get the sum of all the order totals after adding 
+  in the sales tax
+*/
 
-// Below we have an array of purchases
-// We want to create a total for the purcahses, but only want to total Bob's
-// purchases
+let ordersTotal //Code Here
 
+
+
+////////// PROBLEM 6 //////////
+
+// Do not edit the code below.
 const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 {"owner":"Bob","price":73},{"owner":"Barry","price":57},{"owner":"Barry","price":128},
 {"owner":"Bob","price":119},{"owner":"Barry","price":133},{"owner":"Barry","price":27},
 {"owner":"Barry","price":138},{"owner":"Bob","price":68},{"owner":"Bob","price":50},
 {"owner":"Barry","price":9},{"owner":"Bob","price":123},{"owner":"Bob","price":135},
 {"owner":"Barry","price":30},{"owner":"Barry","price":129},{"owner":"Barry","price":38},
-{"owner":"Bob","price":133},{"owner":"Barry","price":109},{"owner":"Bob","price":115}]
+{"owner":"Bob","price":133},{"owner":"Barry","price":109},{"owner":"Bob","price":115}];
+// Do not edit the code above.
 
-let bobsTotal = purchases.reduce((tot,curr)=>{
-    if(curr.owner === "Bob"){
-        return tot + curr.price
-    }
-    else return tot
-},0) // Your code here
+/*
+  Use a high order method to create to get the sum of bobsTotal.
+*/
+
+let bobsTotal //Code Here
